e9a38967168bb1b00422e4a214982904be314d24
Mohak Chugh <me.mohakchugh@gmail.com>
2020-10-02 00:26:41 +0530

    c++ solution for buy and sell stocks ii added

--- Best Time to Buy and Sell Stock II/BestTimetoBuyandSellStockII.cpp
+++ Best Time to Buy and Sell Stock II/BestTimetoBuyandSellStockII.cpp
@@ -1,16 +1,26 @@
-#include <bits/stdc++.h>
-#define MOD 1000000007
-#define loop(i, a, b) for (int i = a; i < b; i++)
-#define ull unsigned long long int
-#define ll long long int
-using namespace std;
-int main()
+class Solution
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie();
-    cout.tie();
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        if (prices.size() == 0)
+            return 0;
 
-    // Write code here
+        int sum = 0, prev = 0;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            if (i == prices.size() - 1)
+            {
+                sum += prices[i] - prices[prev];
+            }
 
-    return 0;
-}
+            else if (prices[i] > prices[i + 1])
+            {
+                sum += prices[i] - prices[prev];
+                prev = i + 1;
+            }
+        }
+
+        return sum;
+    }
+};
